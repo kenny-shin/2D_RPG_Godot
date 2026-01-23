89188c0421c652ace3e479ade85e81750bc163cb
kenny-shin <kenny-shin@users.noreply.github.com>
2026-01-22 23:32:11 -0800

    Add directional attack logic with animations

--- assets/sprites/player/player.gd
+++ assets/sprites/player/player.gd
@@ -9,6 +9,8 @@ enum State {
 
 @export_category("Stats")
 @export var speed: int = 400
+@export var attack_speed: float = 0.6
+
 
 var state: State = State.IDLE
 var move_direction: Vector2 = Vector2.ZERO
@@ -21,9 +23,13 @@ func _ready() -> void:
 	animation_tree.set_active(true)
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		attack()
 
 func _physics_process(_delta: float) -> void:
-	movement_loop()
+	if not state == State.ATTACK:
+		movement_loop()
 	
 	
 func movement_loop() -> void:
@@ -33,7 +39,7 @@ func movement_loop() -> void:
 	set_velocity(motion)
 	move_and_slide()
 
-	if state == State.IDLE or State.RUN:
+	if state == State.IDLE or state == State.RUN:
 		if move_direction.x < -0.01:
 			$Sprite2D.flip_h = true
 		elif move_direction.x > 0.01:
@@ -56,3 +62,33 @@ func update_animation() -> void:
 			animation_playback.travel("run")
 		State.ATTACK:
 			animation_playback.travel("attack")
+			
+			
+func attack() -> void:
+	if state == State.ATTACK:
+		return
+	state = State.ATTACK
+	
+
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	var attack_dir: Vector2 = (mouse_pos - global_position).normalized()
+	$Sprite2D.flip_h = attack_dir.x < 0 and abs(attack_dir.x) >= abs(attack_dir.y)
+	animation_tree.set("parameters/attack/BlendSpace2D/blend_position", attack_dir)
+	update_animation()
+	
+	await get_tree().create_timer(attack_speed).timeout
+	state = State.IDLE
+	
+
+
+
+
+	
+	
+
+
+
+	
+	
+	
+	
