3c55e16e7c6ff1aae3bb1698faa441c34ad3c9e6
kenny-shin <kenny-shin@users.noreply.github.com>
2026-01-22 21:25:31 -0800

    Coded movement to idle/run animations

--- assets/sprites/player/player.gd
+++ assets/sprites/player/player.gd
@@ -1,10 +1,24 @@
 extends CharacterBody2D
 
+enum State {
+	IDLE,
+	RUN,
+	ATTACK,
+	DEAD
+}
 
 @export_category("Stats")
 @export var speed: int = 400
 
+var state: State = State.IDLE
 var move_direction: Vector2 = Vector2.ZERO
+
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var animation_playback: AnimationNodeStateMachinePlayback = $AnimationTree["parameters/playback"]
+
+
+
 
 func _physics_process(_delta: float) -> void:
 	movement_loop()
@@ -14,3 +28,20 @@ func movement_loop() -> void:
 	var motion: Vector2 = move_direction.normalized() * speed
 	set_velocity(motion)
 	move_and_slide()
+
+	if motion != Vector2.ZERO and state == State.IDLE:
+		state = State.RUN
+		update_animation()
+	elif motion == Vector2.ZERO and state == State.RUN:
+		state = State.IDLE
+		update_animation()
+	
+	
+func update_animation() -> void:
+	match state:
+		State.IDLE:
+			animation_playback.travel("idle")
+		State.RUN:
+			animation_playback.travel("run")
+		State.ATTACK:
+			animation_playback.travel("attack")
