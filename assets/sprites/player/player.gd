b9ad6aef85b7b6bed4186789bf20f54373eff053
kenny-shin <kenny-shin@users.noreply.github.com>
2026-01-22 22:25:59 -0800

    fixed directional attacks

--- assets/sprites/player/player.gd
+++ assets/sprites/player/player.gd
@@ -21,8 +21,11 @@ func _ready() -> void:
 	animation_tree.set_active(true)
 
 
+
 func _physics_process(_delta: float) -> void:
 	movement_loop()
+	
+	
 func movement_loop() -> void:
 	move_direction.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
 	move_direction.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
