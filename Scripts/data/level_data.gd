933830bfb19f10ebd48f486e805507b99a8cd1a6
kenny-shin <kenneth.shin05@gmail.com>
2026-02-01 02:15:02 -0800

    Designed the Main Menu UI button.

--- Scripts/data/level_data.gd
+++ Scripts/data/level_data.gd
@@ -4,6 +4,6 @@ const MAX_LEVEL: int =5
 const LEVEL_THRESHOLDS: Array[int] = [
 	500, # level 1 > 2
 	900, # level 2 > 3
-	2500, # level 3 > 4
-	6300, # level 4 > 5
+	1800, # level 3 > 4
+	2100, # level 4 > 5
 ]
