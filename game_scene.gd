3ce80dc82aac454bd1ede3c080db005e6e28056b
kenny-shin <kenneth.shin05@gmail.com>
2026-01-31 21:18:08 -0800

    Added player progression code.

--- game_scene.gd
+++ game_scene.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	var enemy_array: Array = get_tree().get_nodes_in_group("enemies")
+	for i in enemy_array:
+		i.died.connect(experience_gained)
+		
+func experience_gained(exp_gain: int) -> void:
+	if PlayerData.level == LevelData.MAX_LEVEL:
+		return
+	var new_experience: int = PlayerData.experience + exp_gain
+	if new_experience >= LevelData.LEVEL_THRESHHOLDS[PlayerData.level - 1]:
+		level_up(new_experience)
+	else:
+		PlayerData.experience = new_experience
+		
+func level_up(new_experience: int) -> void:
+	print("yay, I got more powerful")
+	new_experience -= LevelData.LEVEL_THRESHOLDS[PlayerData.level - 1]
+	PlayerData.level += 1
+	PlayerData.experience = new_experience 
+	
