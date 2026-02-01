ec4850794f7992427ea609423d06b83da2df2589
kenny-shin <kenneth.shin05@gmail.com>
2026-02-01 03:56:15 -0800

    Connected game scene to the main menu.

--- Scenes/main/scene_handler.gd
+++ Scenes/main/scene_handler.gd
@@ -1,29 +1,42 @@
 extends Node
 
 @export var main_menu_packed: PackedScene
+@export var game_scene_packed: PackedScene
+
+var main_menu_instance: Control = null
+var game_scene_instance: Node2D = null
 
 func _ready() -> void:
 	load_main_menu("game_start")
-	
 
-func load_main_menu(origin: String) -> void:
-	var main_menu: Control = main_menu_packed.instantiate()
-	main_menu.new_game_pressed.connect(new_game)
-	main_menu.settings_pressed.connect(settings_open)
-	main_menu.about_pressed.connect(about_open)
-	main_menu.exit_pressed.connect(exit_game)
-	add_child(main_menu)
-	
+func load_main_menu(_origin: String) -> void:
+	if game_scene_instance:
+		game_scene_instance.queue_free()
+		game_scene_instance = null
+	if main_menu_instance:
+		main_menu_instance.queue_free()
+		main_menu_instance = null
+
+	main_menu_instance = main_menu_packed.instantiate() as Control
+	main_menu_instance.new_game_pressed.connect(new_game)
+	main_menu_instance.settings_pressed.connect(settings_open)
+	main_menu_instance.about_pressed.connect(about_open)
+	main_menu_instance.exit_pressed.connect(exit_game)
+	add_child(main_menu_instance)
+
 func new_game(origin: String) -> void:
-	pass
-	
+	if origin == "main_menu" and main_menu_instance:
+		main_menu_instance.queue_free()
+		main_menu_instance = null
+
+	game_scene_instance = game_scene_packed.instantiate() as Node2D
+	add_child(game_scene_instance)
+
 func settings_open(_origin: String) -> void:
 	pass
-	
+
 func about_open(_origin: String) -> void:
 	pass
-	
+
 func exit_game(_origin: String) -> void:
 	get_tree().quit()
-	
-	
